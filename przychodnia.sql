3ece1ef3d9cb71c62492bf3264ead7465ca3e008
Kbartosz <113676416+Kbartosz@users.noreply.github.com>
2024-11-28 09:32:08 +0100

    Add files via upload

---
+++ przychodnia.sql
@@ -0,0 +1,158 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Lis 28, 2024 at 09:31 AM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `przychodnia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `choroby`
+--
+
+CREATE TABLE `choroby` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `lekarz`
+--
+
+CREATE TABLE `lekarz` (
+  `id` int(11) NOT NULL,
+  `imie` varchar(30) DEFAULT NULL,
+  `nazwisko` varchar(30) DEFAULT NULL,
+  `wiek` int(10) DEFAULT NULL,
+  `haslo` varchar(30) DEFAULT NULL,
+  `mail` varchar(30) DEFAULT NULL,
+  `id_recepty` int(11) DEFAULT NULL,
+  `id_leki` int(11) DEFAULT NULL,
+  `id_wizyt` int(11) DEFAULT NULL,
+  `id_pacjent` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `leki`
+--
+
+CREATE TABLE `leki` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(40) DEFAULT NULL,
+  `id_pacjenta` int(11) DEFAULT NULL,
+  `id_lekarz` int(11) DEFAULT NULL,
+  `id_recepty` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `pacjent`
+--
+
+CREATE TABLE `pacjent` (
+  `pesel` int(11) NOT NULL,
+  `imie` varchar(30) DEFAULT NULL,
+  `nazwisko` varchar(30) DEFAULT NULL,
+  `wiek` int(10) DEFAULT NULL,
+  `haslo` varchar(30) DEFAULT NULL,
+  `mail` varchar(30) DEFAULT NULL,
+  `id_recepty` int(11) DEFAULT NULL,
+  `id_leki` int(11) DEFAULT NULL,
+  `id_wizyt` int(11) DEFAULT NULL,
+  `id_lekarz` int(11) DEFAULT NULL,
+  `id_choroby` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `recepty`
+--
+
+CREATE TABLE `recepty` (
+  `id` int(11) NOT NULL,
+  `data` date DEFAULT NULL,
+  `id_leku` int(11) DEFAULT NULL,
+  `id_pacjenta` int(11) DEFAULT NULL,
+  `id_lekarza` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `wizyty`
+--
+
+CREATE TABLE `wizyty` (
+  `id` int(11) NOT NULL,
+  `data` date DEFAULT NULL,
+  `id_pacjenta` int(11) DEFAULT NULL,
+  `id_lekarza` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `choroby`
+--
+ALTER TABLE `choroby`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `lekarz`
+--
+ALTER TABLE `lekarz`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `leki`
+--
+ALTER TABLE `leki`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `pacjent`
+--
+ALTER TABLE `pacjent`
+  ADD PRIMARY KEY (`pesel`);
+
+--
+-- Indeksy dla tabeli `recepty`
+--
+ALTER TABLE `recepty`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `wizyty`
+--
+ALTER TABLE `wizyty`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
